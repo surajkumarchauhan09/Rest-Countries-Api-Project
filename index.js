7cc0fa594fd940c3464f8ec7443800960225b13a
Suraj Kumar <surajk90201@gmail.com>
2024-09-16 11:38:29 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import React from "react";
+
+const root = createRoot(document.querySelector("#root"));
+
+root.render(<App/>);
